085bb94e8242ca7fc832c597c31f3d017cd98f6e
edumorac <100218106+edumorac@users.noreply.github.com>
2022-05-20 13:23:35 -0400

    Add files via upload

---
+++ My age in dog years.js
@@ -0,0 +1,17 @@
+// this is my age stored in the variable myAge
+let myAge = 34;
+// these are the early years of a dog
+let earlyYears = 2;
+earlyYears *= 10.5;
+// substracting two years to the variable myAge
+let laterYears = myAge - 2;
+// here im multiplying the variable laterYears by 4
+laterYears *= 4;
+// determining my age in dog years and creating a new variable
+myAgeInDogYears = earlyYears + laterYears;
+// creating a variable with my name as string and lower case
+const myName = 'Eduardo' .toLowerCase();
+/*
+using console.log to show my age and my age in dog years
+*/
+console.log(`My name is ${myName}. I am a ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
